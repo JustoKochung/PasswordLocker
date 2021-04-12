5e6771da4b197adc2f44f5db78a6043e9843c0fe
JustoKochung <kochungjusto26@gmail.com>
2021-04-12 11:42:25 +0300

    Add delete, display, save, copy, and create credentials functions in user class

---
+++ user.py
@@ -0,0 +1,52 @@
+import pyperclip
+
+class User:
+    """
+    Class that generates new instances of a user
+    """
+    userList = [] # Empty user list
+    
+    def __init__(self, userName, password):
+        """Function to create a new instance of credentials
+        """
+        self.userName = userName
+        self.password = password
+        
+    def saveUser(self):
+        
+        """Function to store credentials
+        """
+        User.userList.append(self)
+    
+    def deleteUser(self): 
+        
+        """Function to delete credentials
+        """
+        User.userList.remove(self)
+    
+    def display_user():
+        """Function to display all credentials
+        """
+        return User.userList()
+    
+@classmethod
+    def find_credentials_by_userName(cls, userName):
+        for credentials in cls.userList:
+            if credentials.userName == userName
+            return credentials
+        
+        
+@classmethod
+    def copy_password(cls, password):
+        password_found = User.find_credentials_by_userName(password)
+        pyperclip.copy(password_found.password)
+
+@classmethod
+    def copy_userName(cls, userName):
+        username_found = User.find_credentials_by_userName(userName)
+        pyperclip.copy(username_found.userName)
+        
+        
+    
+    
+        
