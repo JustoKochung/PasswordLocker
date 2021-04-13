7df1f05181cf6e47eb1c375a28de85eb904516d8
JustoKochung <kochungjusto26@gmail.com>
2021-04-13 04:24:57 +0300

    Add tearDown method for cleaning test case runs

--- userTest.py
+++ userTest.py
@@ -25,8 +25,23 @@ class TestUser(unittest.TestCase):
         """
         self.new_user.save_user() #saving the new user
         self.assertEqual(len(User.user_list), 1)
-        
+    
+    def tearDown(self):
+        """Method for cleaning up after each test case has run
+        """
+        User.user_list = []
+    
+    def test_save_multiple_user(self):
+        """Test case to check if we can save multiple user objects to user list
+        """
+        self.new_user.save_user()
+        test_user = User("Test", "user", "0799762744") # New User
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
             
+    
+        
+    
     
 if __name__ == '__main__':
     unittest.main()
